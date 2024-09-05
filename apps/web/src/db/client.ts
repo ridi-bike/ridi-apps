18096c052b4c8ff6be75bbfcc4c24e3469297cef
Toms Jansons <toms.jansons@hood-software.lv>
2024-09-05 16:28:35 +0300

    biome lint fixes

--- apps/web/src/db/client.ts
+++ apps/web/src/db/client.ts
@@ -1,6 +1,6 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaLibSQL } from "@prisma/adapter-libsql";
 import { createClient } from "@libsql/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { PrismaClient } from "@prisma/client";
 
 const libsql = createClient({
 	url: `${process.env.TURSO_DATABASE_URL}`,
