e5e7f77016f63d4a08c8572ab3e404ee39d8518d
Toms Jansons <toms.jansons@hood-software.lv>
2024-09-06 17:49:41 +0300

    switch to postgres

--- apps/web/src/server/env.ts
+++ apps/web/src/server/env.ts
@@ -1,9 +1,7 @@
-/* eslint-disable @typescript-eslint/no-empty-interface */
 import { z } from "zod";
 
 const nodeEnvSchema = z.object({
-	TURSO_DATABASE_URL_SYSTEM: z.string().min(5),
-	TURSO_AUTH_TOKEN_SYSTEM: z.string().min(5),
+	DATABASE_URL: z.string().min(5),
 	AUTH_SECRET: z.string().min(5),
 	AUTH_TRUST_HOST: z.coerce.boolean(),
 	GITHUB_CLIENT_ID: z.string().min(5),
@@ -15,9 +13,7 @@ type NodeEnvSchema = z.infer<typeof nodeEnvSchema>;
 export const checkEnvVariables = () => nodeEnvSchema.parse(process.env);
 
 declare global {
-	// eslint-disable-next-line @typescript-eslint/no-namespace
 	namespace NodeJS {
-		// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
-		interface ProcessEnv extends NodeEnvSchema {}
+		interface ProcessEnv extends NodeEnvSchema { }
 	}
 }
--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,29 +1,11 @@
-import { PrismaClient as PrismaUserClient } from "@ridi/generated-user";
-import { PrismaClient as PrismaSystemClient } from "@ridi/generated-system";
+import pg from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
 
-import { createClient } from "@libsql/client";
-import { PrismaLibSQL } from "@prisma/adapter-libsql";
+const { Pool } = pg; // vite needs this
 
-console.log(import.meta.env);
-console.log(process.env);
+const connectionString = `${process.env.DATABASE_URL}`;
 
-const libsql = createClient({
-	url: `${process.env.TURSO_DATABASE_URL_SYSTEM}`,
-	authToken: `${process.env.TURSO_AUTH_TOKEN_SYSTEM}`,
-});
-
-const adapter = new PrismaLibSQL(libsql);
-export const systemDbClient = new PrismaSystemClient({ adapter });
-
-export function constructUserDbClient(
-	url: string,
-	authToken: string,
-): PrismaUserClient {
-	const libsql = createClient({
-		url,
-		authToken,
-	});
-
-	const adapter = new PrismaLibSQL(libsql);
-	return new PrismaUserClient({ adapter });
-}
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+export const prismaClient = new PrismaClient({ adapter });
