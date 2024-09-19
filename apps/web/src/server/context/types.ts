3432257b601d8e8e6f1cedf40bbbab4131121807
Toms Jansons <toms.jansons@hood-software.lv>
2024-09-19 14:51:17 +0300

    fresh expo

--- apps/web/src/server/context/types.ts
+++
@@ -1,7 +0,0 @@
-import "@ridi/server-client-check/server-only";
-import type { prismaClient } from "@ridi/database";
-
-type DbTrans = Parameters<Parameters<typeof prismaClient.$transaction>[0]>[0];
-type DbCon = typeof prismaClient;
-
-export type Db = DbCon | DbTrans;
