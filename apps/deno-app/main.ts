8f1700d3e057264b4d373a24fa6cc88503a4b812
Toms Jansons <toms.jansons@hood-software.lv>
2024-10-13 11:45:48 +0200

    health status

--- apps/deno-app/main.ts
+++ apps/deno-app/main.ts
@@ -1,4 +1,7 @@
-import { createClient } from "jsr:@supabase/supabase-js";
+import {
+	createClient,
+	REALTIME_CHANNEL_STATES,
+} from "jsr:@supabase/supabase-js";
 
 const supabase_url = Deno.env.get("SUPABASE_URL");
 const supabase_key = Deno.env.get("SUPABASE_SECRET_KEY");
@@ -6,10 +9,7 @@ const supabase_key = Deno.env.get("SUPABASE_SECRET_KEY");
 if (!supabase_url || !supabase_key) {
 	throw new Error("missing supabase creds");
 }
-const supabase = createClient(
-	supabase_url,
-	supabase_key,
-);
+const supabase = createClient(supabase_url, supabase_key);
 
 const channel = supabase
 	.channel("realtime_tests")
@@ -23,11 +23,17 @@ const channel = supabase
 	)
 	.subscribe();
 
+console.log(`established connection to ${supabase_url}`);
+
 globalThis.addEventListener("unload", () => {
 	channel.unsubscribe();
 });
 
-Deno.serve(
-	{ port: 2727, hostname: "0.0.0.0" },
-	(_req) => new Response("Hello, world"),
-);
+Deno.serve({ port: 2727, hostname: "0.0.0.0" }, (_req) => {
+	if (channel.state !== REALTIME_CHANNEL_STATES.joined) {
+		return new Response(channel.state, {
+			status: 500,
+		});
+	}
+	return new Response("Hello, world");
+});
