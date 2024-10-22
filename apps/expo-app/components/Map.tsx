deb5b70c7d420b92271b1e33e234d01a8f629259
Toms Jansons <toms.jansons@hood-software.lv>
2024-10-22 17:01:48 +0200

    map libre wip

--- apps/expo-app/components/Map.tsx
+++ apps/expo-app/components/Map.tsx
@@ -5,10 +5,12 @@ import MapLibreGL from "@maplibre/maplibre-react-native";
 // Will be null for most users (only Mapbox authenticates this way).
 // Required on Android. See Android installation notes.
 MapLibreGL.setAccessToken(null);
-
+// MapLibreGL.setConnected(true);
 const styles = StyleSheet.create({
 	page: {
 		flex: 1,
+		width: "100%",
+		height: "100%",
 		justifyContent: "center",
 		alignItems: "center",
 		backgroundColor: "#F5FCFF",
@@ -32,3 +34,36 @@ export class MapsMaps extends Component {
 		);
 	}
 }
+
+// import React from "react";
+// import { StyleSheet, View } from "react-native";
+// import Mapbox from "@rnmapbox/maps";
+//
+// Mapbox.setAccessToken(
+// 	"pk.eyJ1IjoidG9tc2phbnNvbnMiLCJhIjoiY20ya2lwNmNrMDFtZzJxcXlvaTJqNzN0eSJ9.BM-1TkuejUWH03wiF4NpHw",
+// );
+//
+// export const MapsMaps = () => {
+// 	return (
+// 		<View style={styles.page}>
+// 			<View style={styles.container}>
+// 				<Mapbox.MapView style={styles.map} />
+// 			</View>
+// 		</View>
+// 	);
+// };
+//
+// const styles = StyleSheet.create({
+// 	page: {
+// 		flex: 1,
+// 		justifyContent: "center",
+// 		alignItems: "center",
+// 	},
+// 	container: {
+// 		height: 300,
+// 		width: 300,
+// 	},
+// 	map: {
+// 		flex: 1,
+// 	},
+// });
