989db85132006167cc323ad0b6092020193ef831
Toms Jansons <toms.jansons@hood-software.lv>
2024-10-23 12:26:11 +0200

    web maps

--- apps/expo-app/components/Map.web.tsx
+++ apps/expo-app/components/Map.web.tsx
@@ -1,9 +1,11 @@
 import { Map as MapLibre } from "@vis.gl/react-maplibre";
 import "maplibre-gl/dist/maplibre-gl.css";
+import maplibre from 'maplibre-gl'
 
 export function MapsMaps() {
 	return (
 		<MapLibre
+			mapLib={maplibre}
 			initialViewState={{
 				longitude: -100,
 				latitude: 40,
