ced7592a353438cfd08a569bd7c6e75f5cb6482e
Toms Jansons <toms.jansons@hood-software.lv>
2024-10-25 17:30:14 +0200

    creating a track request

--- apps/expo-app/app/track-request/new/index.tsx
+++ apps/expo-app/app/track-request/new/index.tsx
@@ -1,4 +1,3 @@
-import { AnimatePresence } from "@legendapp/motion";
 import { Memo, Show, useObservable } from "@legendapp/state/react";
 import * as Location from "expo-location";
 import { useCallback } from "react";
@@ -7,14 +6,16 @@ import GeoMap from "~/components/geo-map";
 import type { Coords, FindCoords, MapPoint } from "~/components/geo-map/types";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
-import { Label } from "~/components/ui/label";
 import { Text } from "~/components/ui/text";
 import { LocationPermsNotGiven } from "./LocationPermsNotGiven";
+import { tracks$ } from "~/lib/stores";
+import { generate } from "xksuid";
 
 export default function TrackRequestNew() {
 	const showLocationAlert$ = useObservable(false);
-	const fromCoords$ = useObservable<null | Coords>(null);
-	const toCoords$ = useObservable<null | Coords>(null);
+	const startCoords$ = useObservable<null | Coords>(null);
+	const finishCoords$ = useObservable<null | Coords>(null);
+	const currentCoords$ = useObservable<null | Coords>(null);
 	const searchTerm$ = useObservable("");
 	const searchPoints$ = useObservable<MapPoint[]>([]);
 	const findCoords$ = useObservable<null | FindCoords>(null);
@@ -41,106 +42,143 @@ export default function TrackRequestNew() {
 
 	return (
 		<View>
-			<Show if={showLocationAlert$} wrap={AnimatePresence}>
+			<Show if={showLocationAlert$}>
 				<LocationPermsNotGiven
 					close={() => {
 						showLocationAlert$.set(false);
 					}}
 				/>
 			</Show>
-			<View className="flex-row">
-				<Text>From:</Text>
-				<Button
-					variant="outline"
-					onPress={async () => {
-						const { status } =
-							await Location.requestForegroundPermissionsAsync();
-						if (status !== "granted") {
-							showLocationAlert$.set(true);
-							return;
-						}
+			<View className="flex-col">
+				<Text>Show on map:</Text>
+				<View className="flex-row">
+					<Button
+						variant="outline"
+						onPress={async () => {
+							const { status } =
+								await Location.requestForegroundPermissionsAsync();
+							if (status !== "granted") {
+								showLocationAlert$.set(true);
+								return;
+							}
 
-						const location = await Location.getCurrentPositionAsync({});
+							const location = await Location.getCurrentPositionAsync({});
 
-						if (location) {
-							fromCoords$.set({
-								lat: location.coords.latitude,
-								lon: location.coords.longitude,
-							});
-						}
-					}}
-				>
-					<Text>Current location</Text>
-				</Button>
-				<Button
-					variant="outline"
-					onPress={async () => {
-						const { status } =
-							await Location.requestForegroundPermissionsAsync();
-						if (status !== "granted") {
-							findCoords$.set({
-								initialCoords: {
-									lat: 0,
-									lon: 0,
-								},
-								onCoordsChange: (coords) => findCoordsCurrent$.set(coords),
-							});
-							return;
-						}
-
-						const location = await Location.getCurrentPositionAsync({});
-
-						if (location) {
-							findCoords$.set({
-								initialCoords: {
+							if (location) {
+								currentCoords$.set({
 									lat: location.coords.latitude,
 									lon: location.coords.longitude,
-								},
-								onCoordsChange: (coords) => findCoordsCurrent$.set(coords),
-							});
-						} else {
-							findCoords$.set({
-								initialCoords: {
-									lat: 0,
-									lon: 0,
-								},
-								onCoordsChange: (coords) => findCoordsCurrent$.set(coords),
+								});
+							}
+						}}
+					>
+						<Text>Current location</Text>
+					</Button>
+					<Button
+						variant="outline"
+						onPress={async () => {
+							const { status } =
+								await Location.requestForegroundPermissionsAsync();
+							if (status !== "granted") {
+								findCoords$.set({
+									initialCoords: {
+										lat: 0,
+										lon: 0,
+									},
+									onCoordsChange: (coords) => findCoordsCurrent$.set(coords),
+								});
+								return;
+							}
+
+							const location = await Location.getCurrentPositionAsync({});
+
+							if (location) {
+								findCoords$.set({
+									initialCoords: {
+										lat: location.coords.latitude,
+										lon: location.coords.longitude,
+									},
+									onCoordsChange: (coords) => findCoordsCurrent$.set(coords),
+								});
+							} else {
+								findCoords$.set({
+									initialCoords: {
+										lat: 0,
+										lon: 0,
+									},
+									onCoordsChange: (coords) => findCoordsCurrent$.set(coords),
+								});
+							}
+						}}
+					>
+						<Text>Coords finder</Text>
+					</Button>
+				</View>
+				<View className="flex-row">
+					<Input
+						placeholder="Search"
+						$value={searchTerm$}
+						onChangeText={(t) => searchTerm$.set(t)}
+					/>
+					<Button variant="outline" onPress={geoSearch}>
+						<Text>GO</Text>
+					</Button>
+				</View>
+			</View>
+			<View className="flex-col">
+				<View className="flex-row">
+					<Text className="font-bold">Start:</Text>
+					<Text>
+						<Memo>
+							{() => `${startCoords$.lat.get()}, ${startCoords$.lon.get()}`}
+						</Memo>
+					</Text>
+				</View>
+				<View className="flex-row">
+					<Text className="font-bold">Finish:</Text>
+					<Text>
+						<Memo>
+							{() => `${finishCoords$.lat.get()}, ${finishCoords$.lon.get()}`}
+						</Memo>
+					</Text>
+				</View>
+			</View>
+			<View className="flex-row">
+				<Button
+					variant="outline"
+					onPress={() => {
+						const start = startCoords$.get();
+						const finish = finishCoords$.get();
+						if (start && finish) {
+							tracks$.addTrackRequest({
+								id: generate(),
+								name: "",
+								status: "new",
+								from_lat: start.lat,
+								from_lon: start.lon,
+								to_lat: finish.lat,
+								to_lon: finish.lon,
+								user_id: "",
+								created_at: "",
+								modified_at: "",
 							});
 						}
 					}}
 				>
-					<Text>From map</Text>
-				</Button>
-				<Text>
-					<Memo>
-						{() =>
-							fromCoords$.get()
-								? `${fromCoords$.lat.get()}, ${fromCoords$.lon.get()}`
-								: "None"
-						}
-					</Memo>
-				</Text>
-			</View>
-			<View className="flex-row">
-				<Text>To:</Text>
-				<Label nativeID="search-terms">Search</Label>
-				<Input
-					$value={searchTerm$}
-					onChangeText={(t) => searchTerm$.set(t)}
-					nativeID="search-terms"
-				/>
-				<Button variant="outline" onPress={geoSearch}>
-					<Text>GO</Text>
+					<Text>Generate routes</Text>
 				</Button>
 			</View>
 			<View>
 				<Memo>
 					{() => (
 						<GeoMap
-							from={fromCoords$.get()}
-							to={toCoords$.get()}
+							start={startCoords$.get()}
+							finish={finishCoords$.get()}
+							current={currentCoords$.get()}
 							points={searchPoints$.get()}
-							findCoords={findCoords$.get()}
+							findCoords={findCoords$.get() as FindCoords}
+							setStart={(coords) => startCoords$.set(coords)}
+							setFinish={(coords) => finishCoords$.set(coords)}
 						/>
 					)}
 				</Memo>
--- apps/expo-app/lib/stores.ts
+++ apps/expo-app/lib/stores.ts
@@ -1,8 +1,7 @@
-import { observable } from "@legendapp/state";
+import { type Observable, observable } from "@legendapp/state";
 import { ObservablePersistLocalStorage } from "@legendapp/state/persist-plugins/local-storage";
-import { syncedCrud } from "@legendapp/state/sync-plugins/crud";
 import { ObservablePersistMMKV } from "@legendapp/state/persist-plugins/mmkv";
-import { supabase, trpc, trpcClient } from "./supabase";
+import { supabase, trpcClient } from "./supabase";
 import type { AppRouter } from "../../../supabase/functions/trpc/router";
 import { synced } from "@legendapp/state/sync";
 import { Platform } from "react-native";
@@ -19,54 +18,75 @@ export const session$ = observable<{
 type TrackRequest = Awaited<
 	ReturnType<AppRouter["trackRequests"]["list"]>
 >[number];
-type Track = Awaited<ReturnType<AppRouter["tracks"]["list"][number]>>;
+type Track = Awaited<ReturnType<AppRouter["tracks"]["list"]>>[number];
 type Tracks = {
+	addTrackRequest: (trackReq: TrackRequest) => void;
 	tracks: Track[];
 	trackRequests: TrackRequest[];
 };
 
-export const tracks$ = observable(
+function addTrackRequest(trackReq: TrackRequest) {
+	tracks$.trackRequests.set((trs) => [...trs, trackReq]);
+}
+
+export const tracks$ = observable<Tracks>(
 	synced<Tracks>({
-		get: async (params) => {
-			console.log("get", { params });
+		get: async () => {
 			const trackRequests = await trpcClient.trackRequests.list.query();
 			const tracks = await trpcClient.tracks.list.query();
 			return {
+				addTrackRequest,
 				tracks,
 				trackRequests,
 			};
 		},
 		set: async (params) => {
-			if (params.changes.some(change => change.path.length !== 1 || change.path[0] !== "trackRequests")) {
-				throw new Error("unsupported changes")
+			if (
+				params.changes.some(
+					(change) =>
+						change.path.length !== 1 || change.path[0] !== "trackRequests",
+				)
+			) {
+				throw new Error("unsupported changes");
 			}
 			for (const change of params.changes) {
-				const newTrackReq = (change.valueAtPath as TrackRequest[]).find(trRq => !(change.prevAtPath as TrackRequest[]).find(trReqPrev => trReqPrev.id === trRq.id))
+				const newTrackReq = (change.valueAtPath as TrackRequest[]).find(
+					(trRq) =>
+						!(change.prevAtPath as TrackRequest[]).find(
+							(trReqPrev) => trReqPrev.id === trRq.id,
+						),
+				);
 
 				if (!newTrackReq) {
-					throw new Error("wut cant be missing")
+					throw new Error("wut cant be missing");
 				}
 				const trackRequest = await trpcClient.trackRequests.create.mutate({
 					from_lat: newTrackReq.from_lat,
 					from_lon: newTrackReq.from_lon,
 					to_lat: newTrackReq.to_lat,
 					to_lon: newTrackReq.to_lon,
-					name: newTrackReq.name
+					name: newTrackReq.name,
 					// TODO created at from local or from db??
-				})
+				});
 			}
 		},
 		subscribe: ({ refresh }) => {
-			const channel = supabase.realtime.channel("track_requests").on(
-				"postgres_changes",
-				{
-					event: "*",
-					schema: "public",
-				},
-				(_payload) => { console.log("changes", { _payload }); refresh() },
-			).subscribe()
+			const channel = supabase.realtime
+				.channel("track_requests")
+				.on(
+					"postgres_changes",
+					{
+						event: "*",
+						schema: "public",
+					},
+					(_payload) => {
+						console.log("changes", { _payload });
+						refresh();
+					},
+				)
+				.subscribe();
 
-			console.log("subscribe to changes", channel.state)
+			console.log("subscribe to changes", channel.state);
 			return () => channel.unsubscribe();
 		},
 		mode: "merge",
@@ -75,6 +95,7 @@ export const tracks$ = observable(
 		},
 		waitFor: session$.initialized,
 		initial: {
+			addTrackRequest,
 			tracks: [],
 			trackRequests: [],
 		},
