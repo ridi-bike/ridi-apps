c4fffb6e4831dbc7f2b31f3ae44a76e603f47daf
Toms Jansons <toms.jansons@hood-software.lv>
2024-10-28 14:44:17 +0100

    version trpc

--- supabase/functions/trpc/router.ts
+++ supabase/functions/trpc/router.ts
@@ -1,5 +1,7 @@
 import { initTRPC, TRPCError } from "@trpc/server";
 import {
+	array,
+	literal,
 	maxValue,
 	minValue,
 	number,
@@ -8,6 +10,7 @@ import {
 	parser,
 	pipe,
 	string,
+	variant,
 } from "valibot";
 import type { Context } from "./context.ts";
 
@@ -29,47 +32,116 @@ const userProcedure = anonProcedure.use(({ ctx, next }) => {
 const router = t.router;
 
 const tracksRouter = router({
-	list: userProcedure.query(async ({ ctx }) => {
-		const result = await ctx.supabaseClient
-			.from("tracks")
-			.select("*")
-			.order("created_at", {
-				ascending: false,
-			});
-		if (result.error) {
-			throw result.error;
-		}
-		return result.data;
-	}),
+	list: userProcedure
+		.input(parser(variant("version", [object({ version: literal("v1") })])))
+		.output(
+			parser(
+				variant("version", [
+					object({
+						version: literal("v1"),
+						data: array(
+							object({
+								id: string(),
+							}),
+						),
+					}),
+				]),
+			),
+		)
+		.query(async ({ ctx, input: { version } }) => {
+			if (version !== "v1") {
+				throw new Error("wrong version");
+			}
+			const result = await ctx.supabaseClient
+				.from("tracks")
+				.select("*")
+				.filter("user_id", "eq", ctx.user.id)
+				.order("created_at", {
+					ascending: false,
+				});
+			if (result.error) {
+				throw result.error;
+			}
+			return { version: "v1", data: result.data };
+		}),
 });
 
 const trackRequestsRouter = router({
-	list: userProcedure.query(async ({ ctx }) => {
-		const result = await ctx.supabaseClient
-			.from("track_requests")
-			.select("*")
-			.order("created_at", {
-				ascending: false,
-			});
-		if (result.error) {
-			throw result.error;
-		}
-		return result.data;
-	}),
+	list: userProcedure
+		.input(parser(variant("version", [object({ version: literal("v1") })])))
+		.output(
+			parser(
+				variant("version", [
+					object({
+						version: literal("v1"),
+						data: array(
+							object({
+								id: string(),
+							}),
+						),
+					}),
+				]),
+			),
+		)
+		.query(async ({ ctx, input: { version } }) => {
+			if (version !== "v1") {
+				throw new Error("wrong version");
+			}
+			const result = await ctx.supabaseClient
+				.from("track_requests")
+				.select("*")
+				.filter("user_id", "eq", ctx.user.id)
+				.order("created_at", {
+					ascending: false,
+				});
+			if (result.error) {
+				throw result.error;
+			}
+			return {
+				version: "v1",
+				data: result.data,
+			};
+		}),
 	create: userProcedure
 		.input(
 			parser(
-				object({
-					from_lat: pipe(number(), minValue(-90), maxValue(90)),
-					from_lon: pipe(number(), minValue(-180), maxValue(180)),
-					to_lat: pipe(number(), minValue(-90), maxValue(90)),
-					to_lon: pipe(number(), minValue(-180), maxValue(180)),
-					name: optional(string()),
-				}),
+				variant("version", [
+					object({
+						version: literal("v1"),
+						data: object({
+							from_lat: pipe(number(), minValue(-90), maxValue(90)),
+							from_lon: pipe(number(), minValue(-180), maxValue(180)),
+							to_lat: pipe(number(), minValue(-90), maxValue(90)),
+							to_lon: pipe(number(), minValue(-180), maxValue(180)),
+							name: optional(string()),
+						}),
+					}),
+				]),
+			),
+		)
+		.output(
+			parser(
+				variant("version", [
+					object({
+						version: literal("v1"),
+						data: object({
+							id: string(),
+						}),
+					}),
+				]),
 			),
 		)
 		.mutation(
-			async ({ ctx, input: { name, from_lat, to_lon, from_lon, to_lat } }) => {
+			async ({
+				ctx,
+				input: {
+					version,
+					data: { name, from_lat, to_lon, from_lon, to_lat },
+				},
+			}) => {
+				if (version !== "v1") {
+					throw new Error("wrong version");
+				}
 				const result = await ctx.supabaseClient
 					.from("track_requests")
 					.insert({
@@ -89,7 +161,10 @@ const trackRequestsRouter = router({
 				if (!row) {
 					throw new Error("omg");
 				}
-				return row;
+				return {
+					version: "v1",
+					data: row,
+				};
 			},
 		),
 });
