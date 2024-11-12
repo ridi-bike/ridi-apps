23fc746f96b7c5a3997bcda417d70fa881c58b64
Toms Jansons <toms.jansons@hood-software.lv>
2024-11-12 23:37:02 +0200

    router binary name

--- apps/deno-app/startup.sh
+++ apps/deno-app/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run ridi_router with the specified region file
-# ./ridi_router -f "/osm-data/${RIDI_ROUTER_REGION}_latest.osm.pbf"
+# ./ridi-router -f "/osm-data/${RIDI_ROUTER_REGION}_latest.osm.pbf"
 
 # After ridi_router completes, start the Deno application
 deno run --allow-env --allow-net main.ts
