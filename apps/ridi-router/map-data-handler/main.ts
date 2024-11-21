319c85a9a6fe11c1d9f0462a2a13aeee49343d41
Toms Jansons <toms.jansons@hood-software.lv>
2024-11-21 23:15:46 +0200

    chore: further wip

--- apps/ridi-router/map-data-handler/main.ts
+++ apps/ridi-router/map-data-handler/main.ts
@@ -11,6 +11,8 @@ const routerVersion = parse(
 	Deno.env.get("RIDI_ROUTER_VERSION"),
 );
 
+db.handlers.createUpdate("map-data", routerVersion);
+
 const regions = parse(
 	array(string()),
 	JSON.parse(Deno.readTextFileSync(locations.getRegionListFileLoc())),
@@ -22,48 +24,72 @@ async function generateCache(mapDataRecord: MapDataRecord) {
 		args: [
 			"cache",
 			"-i",
-			mapDataRecord.pbfLocation,
+			mapDataRecord.pbf_location,
 			"-c",
-			mapDataRecord.cacheLocation,
+			mapDataRecord.cache_location,
 		],
 	});
 
 	const { code, stdout, stderr } = await cmd.output();
 
-	console.log(new TextDecoder().decode(stdout));
-	console.warn(new TextDecoder().decode(stderr));
+	const stdoutOutput = new TextDecoder().decode(stdout);
+	const stderrOutput = new TextDecoder().decode(stderr);
+	console.log(stdoutOutput);
+	console.warn(stderrOutput);
 	db.mapData.updateRecordReady(mapDataRecord.id);
 	if (code !== 0) {
 		console.error("process failed with code", code);
-		db.mapData.updateRecordError(mapDataRecord.id);
+		db.mapData.updateRecordError(
+			mapDataRecord.id,
+			`stdout: ${stdoutOutput}\n\nstderr: ${stderrOutput}`,
+		);
 	}
 }
 async function processRegionList() {
+	db.handlers.updateRecordProcessing("map-data");
+
 	for (const region in regions) {
 		const remoteMd5Url =
 			`https://download.geofabrik.de/${region}-latest.osm.pbf.md5`;
 		const remoteMd5File = await fetch(remoteMd5Url);
 		const remoteMd5 = (await remoteMd5File.text()).split(" ")[0];
 
-		const currentMapData = db.mapData.getCurrentRecord(region);
-		if (currentMapData) {
-			if (remoteMd5 !== currentMapData.pbfMd5) {
-				await downloadRegion(region, remoteMd5);
+		const nextMapData = db.mapData.getNextRecord(region);
+		if (nextMapData) {
+			if (remoteMd5 !== nextMapData.pbf_md5 || nextMapData.status === "error") {
+				db.mapData.updateRecordDiscarded(nextMapData.id);
+				await downloadRegion(region, remoteMd5, null);
+			} else if (nextMapData.status === "new") {
+				await downloadRegion(region, remoteMd5, nextMapData);
+			} else if (
+				nextMapData.status === "downloaded" ||
+				nextMapData.status === "processing"
+			) {
+				await generateCache(nextMapData);
 			}
 		} else {
-			await downloadRegion(region, remoteMd5);
+			const currentMapData = db.mapData.getCurrentRecord(region);
+			if (currentMapData) {
+				if (remoteMd5 !== currentMapData.pbf_md5) {
+					await downloadRegion(region, remoteMd5, null);
+				}
+			} else {
+				await downloadRegion(region, remoteMd5, null);
+			}
 		}
 	}
 }
 
-async function downloadRegion(region: string, md5: string) {
+async function downloadRegion(
+	region: string,
+	md5: string,
+	nextMapDataRecord: MapDataRecord | null,
+) {
 	const remoteFileUrl =
 		`https://download.geofabrik.de/${region}-latest.osm.pbf`;
 
-	const fileResponse = await fetch(remoteFileUrl);
-
 	const fileLocation = await locations.getPbfFileLoc(region, md5);
-	const mapDataRecord = db.mapData.createNextRecord(
+	const mapDataRecord = nextMapDataRecord || db.mapData.createNextRecord(
 		region,
 		md5,
 		fileLocation,
@@ -71,20 +97,49 @@ async function downloadRegion(region: string, md5: string) {
 		await locations.getCacheDirLoc(region, routerVersion),
 	);
 
-	if (fileResponse.body) {
-		const file = await Deno.open(fileLocation, {
-			write: true,
-			create: true,
-		});
+	try {
+		const fileResponse = await fetch(remoteFileUrl);
 
-		await fileResponse.body.pipeTo(file.writable);
-		db.mapData.updateRecordDownloaded(mapDataRecord.id);
-		cacheProcessorQueue.add(() => generateCache(mapDataRecord));
+		if (fileResponse.body) {
+			const file = await Deno.open(fileLocation, {
+				write: true,
+				create: true,
+			});
+
+			await fileResponse.body.pipeTo(file.writable);
+			db.mapData.updateRecordDownloaded(mapDataRecord.id);
+			cacheProcessorQueue.add(() => generateCache(mapDataRecord));
+		}
+	} catch (err) {
+		console.error(err);
+		db.mapData.updateRecordError(mapDataRecord.id, JSON.stringify(err));
+	}
+}
+
+function checkForHandlerStatus() {
+	const nextRecords = db.mapData.getAllNextRecords();
+	if (
+		regions.every((r) =>
+			nextRecords.find((nr) =>
+				nr.region === r && (nr.status === "ready" || nr.status === "error")
+			)
+		)
+	) {
+		db.handlers.updateRecordIdle("map-data");
+	} else {
+		db.handlers.updateRecordUpdatedAt("map-data");
 	}
 }
 
 processRegionList();
 
-Deno.serve({ port: 2727, hostname: "0.0.0.0" }, async (_req) => {
-	return new Response("Hello, world");
+setInterval(checkForHandlerStatus, 10 * 60 * 1000); // every 10 min
+
+Deno.serve({ port: 2727, hostname: "0.0.0.0" }, (_req) => {
+	const handlerRec = db.handlers.get("map-data");
+	if (handlerRec) {
+		return new Response("ok");
+	} else {
+		return new Response("nok", { status: 400 });
+	}
 });
