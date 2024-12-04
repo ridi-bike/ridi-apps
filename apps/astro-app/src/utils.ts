04b72aacf931ffb6613be68ae7c32bdf5458b9e4
Toms Jansons <toms.jansons@hood-software.lv>
2024-12-04 16:03:41 +0200

    feat: landing page

--- apps/astro-app/src/utils.ts
+++ apps/astro-app/src/utils.ts
@@ -1,20 +1,25 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 // Format the date to a string
-function formatDate(date: Date): string {
+export function formatDate(date: Date): string {
   const options: Intl.DateTimeFormatOptions = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
+    year: "numeric",
+    month: "long",
+    day: "numeric",
   };
 
   return new Date(date).toLocaleDateString(undefined, options);
 }
 
 // Capitalize the first letter
-function capitalize(str: string): string {
-  if (typeof str !== 'string' || str.length === 0) {
+export function capitalize(str: string): string {
+  if (typeof str !== "string" || str.length === 0) {
     return str;
   }
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-export { formatDate, capitalize };
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
