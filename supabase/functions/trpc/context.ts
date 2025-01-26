be655dbbfe879ba9731d4afe9a898b35c1552a6d
Toms Jansons <toms.jansons@hood-software.lv>
2025-01-26 13:08:30 +0200

    chore: debug trpc

--- supabase/functions/trpc/context.ts
+++ supabase/functions/trpc/context.ts
@@ -1,16 +1,23 @@
 import { createClient } from "@supabase/supabase-js";
-import type { Database } from "../../../apps/ridi-router/lib/supabase.ts";
+import type { Database } from "@ridi-router/lib/supabase.ts";
 import type { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import postgres from "postgres";
 import { Messaging } from "@ridi-router/messaging/main.ts";
+import { parse, string } from "valibot";
 
 type ContextCreatorParams = Parameters<
   NonNullable<Parameters<typeof fetchRequestHandler>[0]["createContext"]>
 >[0];
 
-const supabaseUrl = Deno.env.get("SUPABASE_URL") ?? "";
-const supabaseServiceRoleKey = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") ?? "";
-const supabaseDbUrl = Deno.env.get("SUPABASE_DB_URL") ?? "";
+const supabaseUrl = parse(string("supabase url"), Deno.env.get("SUPABASE_URL"));
+const supabaseServiceRoleKey = parse(
+  string("supabase service role key"),
+  Deno.env.get("SUPABASE_SERVICE_ROLE_KEY"),
+);
+const supabaseDbUrl = parse(
+  string("supabase db url"),
+  Deno.env.get("SUPABASE_DB_URL"),
+);
 
 export const createContext = async (
   { req }: ContextCreatorParams,
@@ -27,6 +34,7 @@ export const createContext = async (
   const token = authHeader?.replace("Bearer ", "") || "";
   const { data } = await supabaseClient.auth.getUser(token);
   const user = data.user;
+  console.log({ user });
   const messaging = new Messaging(db, {
     info: (...args: unknown[]) => console.log(...args),
     error: (...args: unknown[]) => console.error(...args),
