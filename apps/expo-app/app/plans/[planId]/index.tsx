57699dcdbd795bceb3ef27d882b0cae9f8b5229f
Toms Jansons <toms.jansons@hood-software.lv>
2025-01-28 23:12:03 +0200

    chore: refactor

--- apps/expo-app/app/plans/[planId]/index.tsx
+++ apps/expo-app/app/plans/[planId]/index.tsx
@@ -1,9 +1,10 @@
-import { Stack } from "expo-router";
+import { Link } from "expo-router";
 import { MapPin, Navigation } from "lucide-react-native";
-import { View, Text, ScrollView } from "react-native";
+import { View, Text } from "react-native";
 
 import { RouteCard } from "~/components/route-card";
-import { ScreenHeader } from "~/components/screen-header";
+import { ScreenCard } from "~/components/screen-card";
+import { ScreenFrame } from "~/components/screen-frame";
 
 export default function PlanDetails() {
   const plan = {
@@ -53,65 +54,63 @@ export default function PlanDetails() {
   };
 
   return (
-    <ScrollView className="min-h-screen w-full bg-white dark:bg-gray-900">
-      <View
-        role="main"
-        className="flex flex-row justify-center px-6 pb-24 pt-8 md:px-8"
-      >
-        <Stack.Screen
-          options={{
-            header: (props) => (
-              <ScreenHeader headerProps={props} title="Plan routes" />
-            ),
-          }}
+    <ScreenFrame title="Plan routes">
+      <View className="mx-2 max-w-3xl flex-1">
+        <ScreenCard
+          middle={
+            <>
+              <View className="space-y-4">
+                <View className="flex flex-row items-start gap-3">
+                  <MapPin className="mt-1 size-6 text-[#FF5937]" />
+                  <View>
+                    <Text className="text-sm font-bold text-[#FF5937]">
+                      Start
+                    </Text>
+                    <Text className="text-base font-medium dark:text-gray-200">
+                      {plan.startAddress}
+                    </Text>
+                  </View>
+                </View>
+                <View className="flex flex-row items-start gap-3">
+                  <Navigation className="mt-1 size-6 text-[#FF5937]" />
+                  <View>
+                    <Text className="text-sm font-bold text-[#FF5937]">
+                      End
+                    </Text>
+                    <Text className="text-base font-medium dark:text-gray-200">
+                      {plan.endAddress}
+                    </Text>
+                  </View>
+                </View>
+              </View>
+            </>
+          }
+          bottom={
+            <>
+              <Text className="font-bold dark:text-gray-100">
+                Total distance:{" "}
+              </Text>
+              <Text className="font-medium dark:text-gray-200">
+                {plan.distance}
+              </Text>
+            </>
+          }
         />
-        <View className="mx-2 max-w-3xl flex-1">
-          <View className="mb-8 rounded-2xl border-2 border-black bg-white p-6 shadow-lg dark:border-gray-700 dark:bg-gray-800">
-            <View className="space-y-4">
-              <View className="flex flex-row items-start gap-3">
-                <MapPin className="mt-1 size-6 text-[#FF5937]" />
-                <View>
-                  <Text className="text-sm font-bold text-[#FF5937]">
-                    Start
-                  </Text>
-                  <Text className="text-base font-medium dark:text-gray-200">
-                    {plan.startAddress}
-                  </Text>
-                </View>
-              </View>
-              <View className="flex flex-row items-start gap-3">
-                <Navigation className="mt-1 size-6 text-[#FF5937]" />
-                <View>
-                  <Text className="text-sm font-bold text-[#FF5937]">End</Text>
-                  <Text className="text-base font-medium dark:text-gray-200">
-                    {plan.endAddress}
-                  </Text>
-                </View>
-              </View>
-              <View className="border-t-2 border-black pt-4 text-base dark:border-gray-700">
-                <Text className="font-bold dark:text-gray-100">
-                  Total distance:{" "}
-                </Text>
-                <Text className="font-medium dark:text-gray-200">
-                  {plan.distance}
-                </Text>
-              </View>
-            </View>
-          </View>
-          <Text
-            role="heading"
-            aria-level={2}
-            className="mb-6 text-2xl font-bold dark:text-gray-100"
-          >
-            Available Routes
-          </Text>
-          <View className="grid grid-cols-1 gap-6 md:grid-cols-2">
-            {plan.routes.map((route) => (
-              <RouteCard key={route.id} route={route} />
-            ))}
-          </View>
+        <Text
+          role="heading"
+          aria-level={2}
+          className="my-6 text-2xl font-bold dark:text-gray-100"
+        >
+          Available Routes
+        </Text>
+        <View className="grid grid-cols-1 gap-6 md:grid-cols-2">
+          {plan.routes.map((route) => (
+            <Link key={route.id} href="/plans/1/1">
+              <RouteCard route={route} />
+            </Link>
+          ))}
         </View>
       </View>
-    </ScrollView>
+    </ScreenFrame>
   );
 }
