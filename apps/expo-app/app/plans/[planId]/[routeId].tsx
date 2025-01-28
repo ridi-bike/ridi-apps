dbb58a2c4f394bada54b0dfd3ffdcb58e40007dd
Toms Jansons <toms.jansons@hood-software.lv>
2025-01-28 23:50:36 +0200

    chore: refactor

--- apps/expo-app/app/plans/[planId]/[routeId].tsx
+++ apps/expo-app/app/plans/[planId]/[routeId].tsx
@@ -1,26 +1,171 @@
-import { useLocalSearchParams } from "expo-router";
-import { ScrollView } from "react-native";
+import {
+  Trophy,
+  Navigation,
+  CirclePlayIcon,
+  CirclePauseIcon,
+} from "lucide-react-native";
+import { View, Text } from "react-native";
 
-import GeoMap from "~/components/geo-map/geo-map-coords-selector";
-import { Text } from "~/components/ui/text";
-import { useStoreRoute } from "~/lib/stores/routes-store";
+import { GeoMapStatic } from "~/components/geo-map/geo-map-static";
+import { ScreenCard } from "~/components/screen-card";
+import { ScreenFrame } from "~/components/screen-frame";
 
-export default function RoutePage() {
-	const { routeId } = useLocalSearchParams<{ routeId: string }>();
-	const { data, error, status } = useStoreRoute(routeId);
-	return (
-		<ScrollView className="size-full flex-col">
-			<Text>{`Route ${routeId}`}</Text>
-			<Text>{status}</Text>
-			<Text>{error?.message}</Text>
-			<GeoMap
-				route={data?.data.latLonArray.map((p) => ({ lat: p[0], lon: p[1] }))}
-			/>
-			{data?.data.latLonArray.map((p) => (
-				<Text key={p.join(",")}>
-					{p[0]}, {p[1]}
-				</Text>
-			))}
-		</ScrollView>
-	);
+const route = {
+  name: "Downtown Loop",
+  distance: "3.2 miles",
+  startCoords: [37.7749, -122.4194] as [number, number],
+  finishCoords: [37.7937, -122.3965] as [number, number],
+  roadTypes: {
+    paved: 70,
+    gravel: 20,
+    trail: 10,
+  },
+  surfaceTypes: {
+    smooth: 65,
+    moderate: 25,
+    rough: 10,
+  },
+  score: 85,
+};
+export default function RouteDetails() {
+  return (
+    <ScreenFrame title="Route details">
+      <View className="mx-2 max-w-3xl flex-1 gap-4">
+        <ScreenCard
+          topClassName="h-[65vh]"
+          top={
+            <GeoMapStatic
+              points={[
+                {
+                  icon: <CirclePlayIcon className="size-8 text-green-500" />,
+                  coords: route.startCoords,
+                },
+                {
+                  icon: <CirclePauseIcon className="size-8 text-red-500" />,
+                  coords: route.finishCoords,
+                },
+              ]}
+            />
+          }
+          middle={
+            <>
+              <View className="flex flex-row items-center justify-between">
+                <Text className="text-lg font-bold dark:text-gray-200">
+                  {route.name}
+                </Text>
+                <View className="flex flex-row items-center gap-2">
+                  <Trophy className="size-5 text-[#FF5937]" />
+                  <Text className="font-bold text-[#FF5937]">
+                    {route.score}
+                  </Text>
+                </View>
+              </View>
+            </>
+          }
+          bottom={
+            <>
+              <Navigation className="size-5 text-[#FF5937]" />
+              <Text className="font-bold dark:text-gray-200">
+                {route.distance}
+              </Text>
+            </>
+          }
+        />
+        <ScreenCard
+          middle={
+            <View>
+              <Text
+                role="heading"
+                aria-level={2}
+                className="mb-4 text-lg font-bold dark:text-gray-200"
+              >
+                Road Type Breakdown
+              </Text>
+              <View className="flex flex-row gap-2 text-sm">
+                <View className="flex-1">
+                  <View
+                    className="mb-1 h-2 rounded-full bg-[#FF5937]"
+                    style={{
+                      width: `${route.roadTypes.paved}%`,
+                    }}
+                  />
+                  <Text className="dark:text-gray-200">
+                    Paved {route.roadTypes.paved}%
+                  </Text>
+                </View>
+                <View className="flex-1">
+                  <View
+                    className="mb-1 h-2 rounded-full bg-[#FFA37F]"
+                    style={{
+                      width: `${route.roadTypes.gravel}%`,
+                    }}
+                  />
+                  <Text className="dark:text-gray-200">
+                    Gravel {route.roadTypes.gravel}%
+                  </Text>
+                </View>
+                <View className="flex-1">
+                  <View
+                    className="mb-1 h-2 rounded-full bg-[#FFD7C9]"
+                    style={{
+                      width: `${route.roadTypes.trail}%`,
+                    }}
+                  />
+                  <Text className="dark:text-gray-200">
+                    Trail {route.roadTypes.trail}%
+                  </Text>
+                </View>
+              </View>
+            </View>
+          }
+          bottom={
+            <View>
+              <Text
+                role="heading"
+                aria-level={2}
+                className="mb-4 text-lg font-bold dark:text-gray-200"
+              >
+                Surface Type Breakdown
+              </Text>
+              <View className="flex flex-row gap-2 text-sm">
+                <View className="flex-1">
+                  <View
+                    className="mb-1 h-2 rounded-full bg-[#FF5937]"
+                    style={{
+                      width: `${route.surfaceTypes.smooth}%`,
+                    }}
+                  />
+                  <Text className="dark:text-gray-200">
+                    Smooth {route.surfaceTypes.smooth}%
+                  </Text>
+                </View>
+                <View className="flex-1">
+                  <View
+                    className="mb-1 h-2 rounded-full bg-[#FFA37F]"
+                    style={{
+                      width: `${route.surfaceTypes.moderate}%`,
+                    }}
+                  />
+                  <Text className="dark:text-gray-200">
+                    Moderate {route.surfaceTypes.moderate}%
+                  </Text>
+                </View>
+                <View className="flex-1">
+                  <View
+                    className="mb-1 h-2 rounded-full bg-[#FFD7C9]"
+                    style={{
+                      width: `${route.surfaceTypes.rough}%`,
+                    }}
+                  />
+                  <Text className="dark:text-gray-200">
+                    Rough {route.surfaceTypes.rough}%
+                  </Text>
+                </View>
+              </View>
+            </View>
+          }
+        />
+      </View>
+    </ScreenFrame>
+  );
 }
--- apps/expo-app/components/screen-card.tsx
+++ apps/expo-app/components/screen-card.tsx
@@ -1,25 +1,40 @@
 import { View } from "react-native";
 
+import { cn } from "~/lib/utils";
+
 type ScreenCardProps = {
   top?: React.ReactNode;
+  topClassName?: string;
   middle?: React.ReactNode;
   bottom?: React.ReactNode;
 };
 
-export function ScreenCard({ top, middle, bottom }: ScreenCardProps) {
+export function ScreenCard({
+  top,
+  topClassName,
+  middle,
+  bottom,
+}: ScreenCardProps) {
   return (
     <View
       role="article"
-      className="w-full rounded-2xl border-2 border-black bg-white p-6 shadow-lg dark:border-gray-700 dark:bg-gray-800"
+      className={
+        "w-full rounded-2xl border-2 border-black bg-white p-6 shadow-lg dark:border-gray-700 dark:bg-gray-800"
+      }
     >
       {!!top && (
-        <View className="mb-6 h-56 overflow-hidden rounded-xl border-2 border-black dark:border-gray-700">
+        <View
+          className={cn(
+            "mb-6 h-56 overflow-hidden rounded-xl border-2 border-black dark:border-gray-700",
+            topClassName,
+          )}
+        >
           {top}
         </View>
       )}
       {!!middle && <View className="mb-4">{middle}</View>}
       {!!bottom && (
-        <View className="flex flex-row border-t-2 border-black pt-4 text-base dark:border-gray-700">
+        <View className="border-t-2 border-black pt-4 text-base dark:border-gray-700">
           {bottom}
         </View>
       )}
