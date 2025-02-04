98efef8304146862c50f9f2df24fcdd0c94e8980
Toms Jansons <toms.jansons@hood-software.lv>
2025-02-04 17:31:04 +0200

    fix: plan layout

--- apps/expo-app/app/plans/[planId]/index.tsx
+++ apps/expo-app/app/plans/[planId]/index.tsx
@@ -1,57 +1,37 @@
-import { Link } from "expo-router";
+import { Link, useLocalSearchParams } from "expo-router";
 import { MapPin, Navigation } from "lucide-react-native";
 import { View, Text } from "react-native";
 
+import { getCardinalDirection } from "~/components/geo-map/util";
 import { RouteCard } from "~/components/route-card";
 import { ScreenCard } from "~/components/screen-card";
 import { ScreenFrame } from "~/components/screen-frame";
+import { useStorePlans } from "~/lib/stores/plans-store";
 
 export default function PlanDetails() {
-  const plan = {
-    startAddress: "123 Main St, San Francisco, CA",
-    endAddress: "456 Market St, San Francisco, CA",
-    distance: "3.2 miles",
-    startCoords: { lat: 37.7749, lon: -122.4194 },
-    finishCoords: { lat: 37.7937, lon: -122.3965 },
-    routes: [
-      {
-        id: 1,
-        distance: "3.2 miles",
-        startCoords: { lat: 37.7749, lon: -122.4194 },
-        finishCoords: { lat: 37.7937, lon: -122.3965 },
-        roadTypes: {
-          paved: 70,
-          gravel: 20,
-          trail: 10,
-        },
-        score: 85,
-      },
-      {
-        id: 2,
-        distance: "3.5 miles",
-        startCoords: { lat: 37.7749, lon: -122.4194 },
-        finishCoords: { lat: 37.7937, lon: -122.3965 },
-        roadTypes: {
-          paved: 90,
-          gravel: 10,
-          trail: 0,
-        },
-        score: 75,
-      },
-      {
-        id: 3,
-        distance: "3.8 miles",
-        startCoords: { lat: 37.7749, lon: -122.4194 },
-        finishCoords: { lat: 37.7937, lon: -122.3965 },
-        roadTypes: {
-          paved: 60,
-          gravel: 30,
-          trail: 10,
-        },
-        score: 92,
-      },
-    ],
-  };
+  const { planId } = useLocalSearchParams();
+  const { data: plans } = useStorePlans();
+  const plan = plans.find((p) => p.id === planId);
+
+  if (!plan) {
+    return (
+      <ScreenFrame title="Plan routes">
+        <View className="mx-2 max-w-5xl flex-1">
+          <ScreenCard
+            middle={
+              <View className="flex w-full flex-row items-center justify-center">
+                <Text className="dark:text-gray-200">
+                  Plan with id
+                  <Text className=" px-2 text-gray-500">{planId}</Text> is not
+                  found
+                </Text>
+              </View>
+            }
+          />
+        </View>
+      </ScreenFrame>
+    );
+  }
 
   return (
     <ScreenFrame title="Plan routes">
@@ -67,31 +47,49 @@ export default function PlanDetails() {
                       Start
                     </Text>
                     <Text className="text-base font-medium dark:text-gray-200">
-                      {plan.startAddress}
+                      {plan.startDesc}
                     </Text>
                   </View>
                 </View>
-                <View className="flex flex-row items-start gap-3">
-                  <Navigation className="mt-1 size-6 text-[#FF5937]" />
-                  <View>
-                    <Text className="text-sm font-bold text-[#FF5937]">
-                      End
-                    </Text>
-                    <Text className="text-base font-medium dark:text-gray-200">
-                      {plan.endAddress}
-                    </Text>
+                {plan.tripType === "start-finish" && (
+                  <View className="flex flex-row items-start gap-3">
+                    <Navigation className="mt-1 size-6 text-[#FF5937]" />
+                    <View>
+                      <Text className="text-sm font-bold text-[#FF5937]">
+                        Finish
+                      </Text>
+                      <Text className="text-base font-medium dark:text-gray-200">
+                        {plan?.finishDesc}
+                      </Text>
+                    </View>
                   </View>
-                </View>
+                )}
+                {plan.tripType === "round-trip" && (
+                  <View className="flex flex-row items-start gap-3">
+                    <Navigation className="mt-1 size-6 text-[#FF5937]" />
+                    <View>
+                      <Text className="text-sm font-bold text-[#FF5937]">
+                        Direction
+                      </Text>
+                      <Text className="text-base font-medium dark:text-gray-200">
+                        {getCardinalDirection(plan.bearing!)} ({plan.bearing}
+                        °)
+                      </Text>
+                    </View>
+                  </View>
+                )}
               </View>
             </>
           }
           bottom={
             <>
               <Text className="font-bold dark:text-gray-100">
-                Total distance:{" "}
+                {plan.tripType === "round-trip"
+                  ? "Target distance"
+                  : "Straigt line distance"}
               </Text>
               <Text className="font-medium dark:text-gray-200">
-                {plan.distance}
+                {plan.distance / 1000}km
               </Text>
             </>
           }
@@ -105,7 +103,10 @@ export default function PlanDetails() {
         </Text>
         <View className="grid grid-cols-1 gap-6 md:grid-cols-3">
           {plan.routes.map((route) => (
-            <Link key={route.id} href="/plans/1/1">
+            <Link
+              key={route.routeId}
+              href={`/plans/${plan.id}/${route.routeId}`}
+            >
               <RouteCard route={route} />
             </Link>
           ))}
