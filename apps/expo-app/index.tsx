7f69525cdf6a91cb03b52d432c686c308f19f04a
Toms Jansons <toms.jansons@hood-software.lv>
2025-02-11 16:51:44 +0200

    chore: set up moti

---
+++ apps/expo-app/index.tsx
@@ -0,0 +1,14 @@
+import "./global.css";
+import "react-native-reanimated";
+import "react-native-gesture-handler";
+
+import { registerRootComponent } from 'expo';
+import { ExpoRoot } from 'expo-router';
+
+// Must be exported or Fast Refresh won't update the context
+export function App() {
+  const ctx = require.context('./app');
+  return <ExpoRoot context={ctx} />;
+}
+
+registerRootComponent(App);
