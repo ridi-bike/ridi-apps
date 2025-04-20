8a331c6484e8bd722dc3a0a2fb5e43967e8bc425
Toms Jansons <toms.jansons@hood-software.lv>
2025-04-21 01:31:12 +0300

    feat: pbf wip

--- services/geo-boundary-init/src/main.rs
+++ services/geo-boundary-init/src/main.rs
@@ -1,19 +1,37 @@
-use std::io;
+use std::{io, path::PathBuf, process};
 
-use tracing::Level;
+use tracing::{error, trace, Level};
 
 mod db;
 mod pbf_reader;
 
 fn main() {
     let subscriber = tracing_subscriber::fmt()
-        .json()
+        // .json()
         .with_writer(io::stderr)
         .with_file(true)
         .with_line_number(true)
         .with_thread_names(true)
-        .with_max_level(Level::INFO)
+        .with_max_level(Level::TRACE)
         .finish();
 
-    tracing::subscriber::set_global_default(subscriber);
+    match tracing::subscriber::set_global_default(subscriber) {
+        Err(e) => {
+            error!(error = ?e, "Failed to set up tracing");
+            process::exit(1);
+        }
+        Ok(_) => {}
+    };
+
+    let boundaries = match pbf_reader::pbf_get_boundaries(&PathBuf::from(
+        "../.ridi-data/map-data/latvia/map.osm.pbf",
+    )) {
+        Err(e) => {
+            error!(error = ?e, "Failed to read boundaries");
+            process::exit(1);
+        }
+        Ok(b) => b,
+    };
+
+    trace!(boundaries = ?boundaries, "boundaries found");
 }
--- services/geo-boundary-init/src/pbf_reader.rs
+++ services/geo-boundary-init/src/pbf_reader.rs
@@ -1,19 +1,27 @@
-use std::{io, path::PathBuf, time::Instant};
+use std::{fmt::Display, io, path::PathBuf, time::Instant};
 
+use osmpbfreader::OsmObj;
 use tracing::trace;
 
+#[derive(Debug)]
 pub enum PbfReaderError {
     PbfFileOpenError { error: io::Error },
     PbfFileReadError { error: osmpbfreader::Error },
     NotARelation,
+    NotANode,
+    NotAWay,
     NameNotFound,
-    LevelNotFound,
+    LevelNotFound { id: i64 },
+    WayNotFound,
+    NodeNotFound,
 }
 
+#[derive(Debug)]
 pub struct Boundary {
     id: i64,
     name: String,
-    polygon: Vec<(f64, f64)>,
+    polygons_outer: Vec<Vec<(f64, f64)>>,
+    polygons_inner: Vec<Vec<(f64, f64)>>,
     level: String,
 }
 
@@ -25,34 +33,129 @@ pub fn pbf_get_boundaries(file: &PathBuf) -> Result<Vec<Boundary>, PbfReaderErro
         std::fs::File::open(path).map_err(|error| PbfReaderError::PbfFileOpenError { error })?;
     let mut pbf = osmpbfreader::OsmPbfReader::new(r);
 
-    let boundaries = pbf
-        .get_objs_and_deps(|obj| {
-            obj.is_relation()
-                && obj
-                    .tags()
-                    .iter()
-                    .any(|t| t.0 == "boundary" && t.1 == "administrative")
-        })
-        .map_err(|error| PbfReaderError::PbfFileReadError { error })?
+    let mut nodes: Vec<OsmObj> = Vec::new();
+    let mut ways: Vec<OsmObj> = Vec::new();
+    let mut relations: Vec<OsmObj> = Vec::new();
+
+    pbf.get_objs_and_deps(|obj| {
+        obj.is_relation()
+            && obj.tags().contains("type", "boundary")
+            && obj.tags().contains("boundary", "administrative")
+            && obj.tags().contains_key("admin_level")
+    })
+    .map_err(|error| PbfReaderError::PbfFileReadError { error })?
+    .into_iter()
+    .for_each(|(_id, element)| {
+        trace!(element = ?element, "Element");
+        if element.is_relation() {
+            relations.push(element);
+        } else if element.is_way() {
+            ways.push(element);
+        } else if element.is_node() {
+            nodes.push(element);
+        }
+    });
+
+    let boundaries: Vec<Boundary> = relations
         .iter()
-        .map(|(id, rel)| {
-            if !rel.is_relation() {
+        .map(|element| {
+            if !element.is_relation() {
                 return Err(PbfReaderError::NotARelation);
             }
-            let tags_iter = rel.tags().iter();
+            let relation = match element.relation() {
+                None => return Err(PbfReaderError::NotARelation),
+                Some(rel) => rel,
+            };
+            let mut tags_iter = element.tags().iter();
             let name = match tags_iter.find(|tag| tag.0 == "name").map(|t| t.1) {
                 None => return Err(PbfReaderError::NameNotFound),
                 Some(n) => n,
             };
-            let level = match tags_iter.find(|tag| tag.0 == "name").map(|t| t.1) {
-                None => return Err(PbfReaderError::LevelNotFound),
+            let level = match tags_iter.find(|tag| tag.0 == "admin_level").map(|t| t.1) {
+                None => {
+                    return Err(PbfReaderError::LevelNotFound {
+                        id: element.id().inner_id(),
+                    })
+                }
                 Some(n) => n,
             };
+
+            let border_outer_points = relation
+                .refs
+                .iter()
+                .filter(|relation_ref| relation_ref.role == "outer")
+                .map(|relation_ref| {
+                    let way_id = relation_ref
+                        .member
+                        .way()
+                        .map_or(Err(PbfReaderError::NotAWay), |way| Ok(way))?;
+                    let way = ways
+                        .iter()
+                        .find(|w| w.id().inner_id() == way_id.0)
+                        .map_or(Err(PbfReaderError::WayNotFound), |w| Ok(w))?
+                        .way()
+                        .map_or(Err(PbfReaderError::NotAWay), |w| Ok(w))?;
+                    let points = way
+                        .nodes
+                        .iter()
+                        .map(|p| {
+                            nodes
+                                .iter()
+                                .find(|node| node.id().inner_id() == p.0)
+                                .map_or(Err(PbfReaderError::NodeNotFound), |node| Ok(node))
+                                .map(|node| {
+                                    node.node().map_or(Err(PbfReaderError::NotANode), |n| {
+                                        Ok((n.lat(), n.lon()))
+                                    })
+                                })?
+                        })
+                        .collect::<Result<Vec<_>, PbfReaderError>>()?;
+
+                    Ok(points)
+                })
+                .collect::<Result<Vec<Vec<_>>, PbfReaderError>>()?;
+
+            let border_inner_points = relation
+                .refs
+                .iter()
+                .filter(|relation_ref| relation_ref.role == "inner")
+                .map(|relation_ref| {
+                    let way_id = relation_ref
+                        .member
+                        .way()
+                        .map_or(Err(PbfReaderError::NotAWay), |way| Ok(way))?;
+                    let way = ways
+                        .iter()
+                        .find(|w| w.id().inner_id() == way_id.0)
+                        .map_or(Err(PbfReaderError::WayNotFound), |w| Ok(w))?
+                        .way()
+                        .map_or(Err(PbfReaderError::NotAWay), |w| Ok(w))?;
+                    let points = way
+                        .nodes
+                        .iter()
+                        .map(|p| {
+                            nodes
+                                .iter()
+                                .find(|node| node.id().inner_id() == p.0)
+                                .map_or(Err(PbfReaderError::NodeNotFound), |node| Ok(node))
+                                .map(|node| {
+                                    node.node().map_or(Err(PbfReaderError::NotANode), |n| {
+                                        Ok((n.lat(), n.lon()))
+                                    })
+                                })?
+                        })
+                        .collect::<Result<Vec<_>, PbfReaderError>>()?;
+
+                    Ok(points)
+                })
+                .collect::<Result<Vec<Vec<_>>, PbfReaderError>>()?;
+
             Ok(Boundary {
-                id: id.inner_id(),
+                id: element.id().inner_id(),
                 name: name.to_string(),
                 level: level.to_string(),
-                polygon,
+                polygons_outer: border_outer_points,
+                polygons_inner: border_inner_points,
             })
         })
         .collect::<Result<Vec<_>, PbfReaderError>>()?;
