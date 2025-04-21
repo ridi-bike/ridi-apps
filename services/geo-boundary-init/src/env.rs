94586c07508fcd850115a7059a461465bae9fc42
Toms Jansons <toms.jansons@hood-software.lv>
2025-04-21 17:18:22 +0300

    feat: local setup

---
+++ services/geo-boundary-init/src/env.rs
@@ -0,0 +1,30 @@
+use std::env;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum RidiEnv {
+    Local,
+    Prod,
+}
+
+pub struct Env {
+    pub pbf_location: String,
+    pub supabase_db_url: String,
+    pub env: RidiEnv,
+}
+
+impl Env {
+    pub fn init() -> Self {
+        for val in env::vars() {
+            println!("val: {val:?}");
+        }
+        Env {
+            pbf_location: env::var("PBF_LOCATION").expect("PBF_LOCATION env variable"),
+            supabase_db_url: env::var("SUPABASE_DB_URL").expect("SUPABASE_DB_URL env variable"),
+            env: if env::var("RIDI_ENV").expect("RIDI_ENV env variable") == "prod" {
+                RidiEnv::Prod
+            } else {
+                RidiEnv::Local
+            },
+        }
+    }
+}
--- services/geo-boundary-init/src/main.rs
+++ services/geo-boundary-init/src/main.rs
@@ -1,37 +1,50 @@
 use std::{io, path::PathBuf, process};
 
+use env::{Env, RidiEnv};
 use tracing::{error, trace, Level};
 
 mod db;
+mod env;
 mod pbf_reader;
 
 fn main() {
-    let subscriber = tracing_subscriber::fmt()
-        // .json()
-        .with_writer(io::stderr)
-        .with_file(true)
-        .with_line_number(true)
-        .with_thread_names(true)
-        .with_max_level(Level::TRACE)
-        .finish();
+    let env = Env::init();
 
-    match tracing::subscriber::set_global_default(subscriber) {
-        Err(e) => {
-            error!(error = ?e, "Failed to set up tracing");
-            process::exit(1);
-        }
-        Ok(_) => {}
+    let subscriber = if env.env == RidiEnv::Prod {
+        let subscriber = tracing_subscriber::fmt()
+            .json()
+            .with_writer(io::stderr)
+            .with_file(true)
+            .with_line_number(true)
+            .with_thread_names(true)
+            .with_max_level(Level::TRACE)
+            .finish();
+
+        tracing::subscriber::set_global_default(subscriber)
+    } else {
+        let subscriber = tracing_subscriber::fmt()
+            .with_writer(io::stderr)
+            .with_file(true)
+            .with_line_number(true)
+            .with_thread_names(true)
+            .with_max_level(Level::INFO)
+            .finish();
+        tracing::subscriber::set_global_default(subscriber)
     };
 
-    let boundaries = match pbf_reader::PbfReader::pbf_get_boundaries(&PathBuf::from(
-        "../.ridi-data/map-data/estonia/map.osm.pbf",
-    )) {
-        Err(e) => {
-            error!(error = ?e, "Failed to read boundaries");
-            process::exit(1);
-        }
-        Ok(b) => b,
-    };
+    if let Err(e) = subscriber {
+        error!(error = ?e, "Failed to set up tracing");
+        process::exit(1);
+    }
+
+    let boundaries =
+        match pbf_reader::PbfReader::pbf_get_boundaries(&PathBuf::from(env.pbf_location)) {
+            Err(e) => {
+                error!(error = ?e, "Failed to read boundaries");
+                process::exit(1);
+            }
+            Ok(b) => b,
+        };
 
     trace!(boundaries = ?boundaries.len(), "boundaries found");
 }
