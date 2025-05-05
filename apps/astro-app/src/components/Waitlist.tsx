92fec87b70e899ae0d5c9239a94ea42ec0020609
Toms Jansons <toms.jansons@hood-software.lv>
2025-05-05 16:42:07 +0300

    fix: mailing list

--- apps/astro-app/src/components/Waitlist.tsx
+++ apps/astro-app/src/components/Waitlist.tsx
@@ -1,36 +1,39 @@
 import { useRef, useState } from "react";
 
 export function Waitlist() {
-	const [submitted, setSubmitted] = useState(false)
-	const emailRef = useRef<HTMLInputElement>(null)
+  const [submitted, setSubmitted] = useState(false);
+  const emailRef = useRef<HTMLInputElement>(null);
 
-	if (submitted) {
-		<div>Thank you</div>
-	}
+  if (submitted) {
+    return <div className="h-28">Added to mailing list, thank you</div>;
+  }
+
   return (
     <form
       onSubmit={async (e) => {
-				setSubmitted(true);
-				e.preventDefault();
-				const formData = new FormData()
-				formData.set("email", emailRef.current?.value || "");
-				formData.set("name", "RIDI_WAITLIST");
-				formData.set("content", "RIDI_WAITLIST");
-				try {
-					await fetch("https://contact-us.toms-jansons.workers.dev", {
-						method: "post",
-						body: formData,
-						redirect: "manual"
-					});
-				} catch {
-				}
+        setSubmitted(true);
+        e.preventDefault();
+        const formData = new FormData();
+        formData.set("email", emailRef.current?.value || "");
+        formData.set("name", "RIDI_WAITLIST");
+        formData.set("content", "RIDI_WAITLIST");
+        try {
+          fetch("https://contact-us.toms-jansons.workers.dev", {
+            method: "post",
+            body: formData,
+            redirect: "manual",
+          });
+          setSubmitted(true);
+        } catch (err) {
+          console.error("Error while submitting:", err);
+        }
       }}
       method="post"
     >
       <input
-        className="appearance-none bg-transparent rounded-[14px] h-12 w-64 mb-4 text-gray-200 focus:outline-none focus:bg-gray-300 focus:text-gray-800 focus:border-gray-400 mr-3 py-2 px-3"
+        className="mb-4 mr-3 h-12 w-64 appearance-none rounded-[14px] bg-transparent px-3 py-2 text-gray-200 focus:border-gray-400 focus:bg-gray-300 focus:text-gray-800 focus:outline-none"
         type="email"
-				ref={emailRef}
+        ref={emailRef}
         name="email"
         placeholder="some@example.com"
         aria-label="Email Address"
@@ -39,10 +42,10 @@ export function Waitlist() {
 
       <button
         type="submit"
-        className="px-[35px] py-5 w-64 bg-zinc-900 hover:bg-white text-white hover:text-black border rounded-[14px] justify-start items-start gap-2.5 inline-flex"
+        className="inline-flex w-64 items-start justify-start gap-2.5 rounded-[14px] border bg-zinc-900 px-[35px] py-5 text-white hover:bg-white hover:text-black"
       >
         <div className="text-center text-xl font-normal leading-7">
-          Sign me up
+          Get notified
         </div>
       </button>
     </form>
