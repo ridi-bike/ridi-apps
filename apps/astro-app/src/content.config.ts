56230b6c05a3f0fbf676c9fdd0d7c657aef29fd1
Toms Jansons <toms.jansons@hood-software.lv>
2025-07-24 17:38:43 +0300

    feat: update 2 post

--- apps/astro-app/src/content.config.ts
+++ apps/astro-app/src/content.config.ts
@@ -2,7 +2,7 @@ import { glob } from "astro/loaders"; // Not available with legacy API
 import { defineCollection, z } from "astro:content";
 
 const articles = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./src/content/articles/" }),
+  loader: glob({ pattern: "**/*.md", base: "./src/content/articles" }),
   schema: z.object({
     title: z.string(),
     publish_date: z.date(),
